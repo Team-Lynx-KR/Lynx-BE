35a9f77fd9a6e9e281b3663322f3b233ae261c70
성인석 <inseok1999@gmail.com>
2025-12-20 09:01:31 +0900

    feat: 유저 관련 모듈, 엔티티, 서비스 추가

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  /** 이메일 */
+  @Column({ unique: true })
+  email: string;
+
+  /** 비밀번호 */
+  @Column()
+  password: string;
+
+  /** 닉네임 */
+  @Column({ unique: true })
+  nickname: string;
+
+  /** 생성일 */
+  @CreateDateColumn()
+  createdAt: Date;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(userData: Partial<User>): Promise<User> {
+    const newUser = this.usersRepository.create(userData);
+    return await this.usersRepository.save(newUser);
+  }
+
+  /** 이메일로 사용자 조회 */
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.usersRepository.findOne({ where: { email } });
+    return user ? user : undefined;
+  }
+}
