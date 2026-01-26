b57ca9d4ddcb08b104f299112cac27126e3cb12f
성인석 <inseok1999@gmail.com>
2026-01-26 09:42:57 +0900

    timezone 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
+// 한국 시간대 설정
+process.env.TZ = 'Asia/Seoul';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
